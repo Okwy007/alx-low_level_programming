e1e6f84fb6e01e57580e2f19b3b48b1b1a30dc6d
“okwy007” <gloryokwuchukwu7020@gmail.com>
2022-06-30 19:55:38 -0700

    Test

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,11 @@
 /**
  * main - Entry point
  * print word to output
- * Return: Always 0 (success)
+ * Return:Always 0 (Success)
  */
+
 int main(void)
 {
-	printf("with proper grammar,but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

7187a18e6a281d952977f1d98a90273568c105df
“okwy007” <gloryokwuchukwu7020@gmail.com>
2022-07-07 19:55:45 -0700

    checking upper case letter

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangles
+ *
+ * @size: size of triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		if (size <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			for (j = 0; j > i; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}

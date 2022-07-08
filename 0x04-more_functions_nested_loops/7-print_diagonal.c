afa610235d36779e0514642eab84bbbcca99c734
“okwy007” <gloryokwuchukwu7020@gmail.com>
2022-07-07 19:19:49 -0700

    checking upper case letter

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ *
+ * @n: the nmber of times a backslash should be printed
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = i - 1; j >= 0; j--)
+			{
+				_putchar(32);
+			/*      _putchar(92);*/
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}

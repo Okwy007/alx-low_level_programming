53ab6b39ba99c018b50f195a66840f9aeb74a712
“okwy007” <gloryokwuchukwu7020@gmail.com>
2022-07-07 19:38:01 -0700

    checking upper case letter

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - fizz buzz test
+ *
+ * Return: always 0;
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0)
+			printf("%s", "Fizz");
+		else if (i % 5 == 0)
+			printf("%s", "Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("%s", "FizzBuzz");
+		else
+			printf("%d", i);
+		if (i < 100)
+			printf("%s", " ");
+	}
+	printf("\n");
+	return (0);
+}

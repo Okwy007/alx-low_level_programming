17a70adc52e3a2da2d766cb18005279ecd2d5c7e
“okwy007” <gloryokwuchukwu7020@gmail.com>
2022-07-07 18:04:21 -0700

    checking upper case letter

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 to 14 10 times
+ */
+
+void more_numbers(void)
+{
+	int b, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b < 10)
+			_putchar((b % 10) + '0');
+			if (b > 10)
+				_putchar((b % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}

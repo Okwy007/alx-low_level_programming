27758ab78e590634f56c8e0ea44d2e3a1b2c66ec
“okwy007” <gloryokwuchukwu7020@gmail.com>
2022-07-14 14:06:56 -0700

    task0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * lenstr - calculates the lenght of a string
+ *
+ *  @str: string whose length is to be calculated
+ *
+ *   Return: length of string
+ */
+
+int lenstr(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int dest_len = lenstr(dest);
+	int src_len = lenstr(src);
+
+	for (i = 0; i < src_len; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+
+	return (dest);
+}

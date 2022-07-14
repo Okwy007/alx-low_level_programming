71e311a19aa4f373018c40bd68f4781aec809216
“okwy007” <gloryokwuchukwu7020@gmail.com>
2022-07-14 07:31:24 -0700

    task8hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <unistd.h>
-
 /**
- * main - prints a standard error
- *
- * Return: Always 1 (Success)
+ * main - a function that prints out a message
+ * Return: returns a value of 1 when it runs successfully
  */
+#include <stdio.h>
+#include <unistd.h>
 int main(void)
 {
-write(2, " and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
